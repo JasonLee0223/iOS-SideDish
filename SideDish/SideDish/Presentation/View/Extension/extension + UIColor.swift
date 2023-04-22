24aeeafeb53a7c2ddbd36cbffde7edbf708901f6
Jason <ggj00125@gmail.com>
2023-04-22 15:38:50 +0900

    Feat: extension + UIColor 구현

---
+++ SideDish/SideDish/Presentation/View/Extension/extension + UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  extension + UIColor.swift
+//  SideDish
+//
+//  Created by Jason on 2023/04/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let launchingBadgeBackground = UIColor(red: 0.0, green: 102/255, blue: 214/255, alpha: 1.0)
+    static let eventBadgeBackground = UIColor(red: 128/255, green: 188/255, blue: 255/255, alpha: 1.0)
+    static let defaultBadgeBackground = UIColor(red: 0.0, green: 122/255, blue: 1.0, alpha: 1.0)
+}
