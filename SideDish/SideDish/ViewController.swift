90ec63a69cbd428a87562e1ca107f6869961c3c0
Jason <ggj00125@gmail.com>
2023-04-22 12:47:25 +0900

    Build: 기본 파일 그룹핑

---
+++ SideDish/SideDish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by Jason on 2023/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
