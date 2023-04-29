b61c5e5953cdcfa15f6fce4120f12169515b31e1
Jason <ggj00125@gmail.com>
2023-04-29 17:58:52 +0900

    Style: decoding 확인 코드 작성

--- SideDish/SideDish/Presentation/HomeViewController.swift
+++ SideDish/SideDish/Presentation/HomeViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let networkService = NetworkService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        Task {
+            try await networkService.request(with: APIEndpoint.supplyFoodInformation(with: APIMagicLiteral.main))
+        }
     }
-
-    
 }
 
