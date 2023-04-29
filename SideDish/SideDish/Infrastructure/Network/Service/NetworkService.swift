0bba2817cfbc96bc764be4bb25c286f561c9cbea
Jason <ggj00125@gmail.com>
2023-04-29 17:58:34 +0900

    Refactor: 필요없는 코드 삭제

--- SideDish/SideDish/Infrastructure/Network/Service/NetworkService.swift
+++ SideDish/SideDish/Infrastructure/Network/Service/NetworkService.swift
@@ -18,29 +18,10 @@ class NetworkService {
     func request<R: Decodable, E: RequestAndRespondable>(with endPoint: E) async throws -> R where E.Response == R {
         
         let urlRequest = try endPoint.getURLRequest()
+        let (data, _) = try await session.data(for: urlRequest)
+        let networkResult: R = try self.decode(with: data)
         
-        let (data, response) = try await session.data(for: urlRequest)
-        
-        guard let httpResponse: HTTPURLResponse = response as? HTTPURLResponse else {
-            throw NetworkError.urlRequest
-        }
-        
-        try self.verify(with: httpResponse)
-        
-        return try self.decode(with: data)
-    }
-    
-    private func verify(with HTTPResponse: HTTPURLResponse) throws {
-        switch HTTPResponse.statusCode {
-        case (300...399):
-            throw HTTPError.redirectionMessages(HTTPResponse.statusCode, HTTPResponse.debugDescription)
-        case (400...499):
-            throw HTTPError.clientErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
-        case (500...599):
-            throw HTTPError.serverErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
-        default:
-            throw HTTPError.networkFailError(HTTPResponse.statusCode)
-        }
+        return networkResult
     }
     
     private func decode<R: Decodable>(with apiData: Data) throws -> R {
