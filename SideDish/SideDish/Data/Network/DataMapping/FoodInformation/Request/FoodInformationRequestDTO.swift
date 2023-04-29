cf815d49438f36c10093fa136d1ee2aafa8d3ac1
Jason <ggj00125@gmail.com>
2023-04-29 16:09:56 +0900

    Feat: FoodInformationRequestDTO 타입 구현
    기존 DTO는 Response 그룹으로 이동

---
+++ SideDish/SideDish/Data/Network/DataMapping/FoodInformation/Request/FoodInformationRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  FoodInformationRequestDTO.swift
+//  SideDish
+//
+//  Created by Jason on 2023/04/29.
+//
+
+import Foundation
+
+struct FoodInformationRequestDTO {
+    let path: String
+    let subPath: String
+}
