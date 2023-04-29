4bbf091d6e63cc1daba0d6917985332271d4753d
Jason <ggj00125@gmail.com>
2023-04-29 16:36:29 +0900

    Feat: APIMagicLiteral 타입 구현

---
+++ SideDish/SideDish/Infrastructure/Network/Common/APIMagicLiteral.swift
@@ -0,0 +1,16 @@
+//
+//  APIMagicLiteral.swift
+//  SideDish
+//
+//  Created by Jason on 2023/04/29.
+//
+
+import Foundation
+
+enum APIMagicLiteral {
+    static let baseURL: String = "https://api.codesquad.kr/onban/"
+    static let main: String = "main"
+    static let soup: String = "soup"
+    static let side: String = "side"
+    static let detail: String = "detail"
+}
