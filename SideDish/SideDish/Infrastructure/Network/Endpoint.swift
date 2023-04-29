6cd3527499d28de3f06ccc2509a7d4343063a6ac
Jason <ggj00125@gmail.com>
2023-04-29 15:08:10 +0900

    Feat: Endpoint 클래스 타입 쿠현

---
+++ SideDish/SideDish/Infrastructure/Network/Endpoint.swift
@@ -0,0 +1,33 @@
+//
+//  Endpoint.swift
+//  SideDish
+//
+//  Created by Jason on 2023/04/29.
+//
+
+import Foundation
+
+protocol RequestAndRespondable: Requestable, Respondable {}
+
+class Endpoint<R>: RequestAndRespondable {
+    typealias Response = R
+    
+    var baseURL: String
+    var path: String
+    var subPath: String?
+    var method: HTTPMethod
+    
+    init(baseURL: String,
+         path: String,
+         subPath: String? = nil,
+         method: HTTPMethod = .get) {
+        self.baseURL = baseURL
+        self.path = path
+        self.subPath = subPath
+        self.method = method
+    }
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
