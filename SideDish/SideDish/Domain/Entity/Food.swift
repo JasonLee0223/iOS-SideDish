2855189d540f20b4aa6dee6b04864f19fb5eddff
Jason <ggj00125@gmail.com>
2023-05-03 20:52:51 +0900

    Refactor: Hashable 채택 및 이니셜라이저 코드 삭제
    - class -> struct
    - DiffableDataSource 사용으로 인한 타입 변경

--- SideDish/SideDish/Domain/Entity/Food.swift
+++ SideDish/SideDish/Domain/Entity/Food.swift
@@ -8,19 +8,10 @@
 import Foundation
 import UIKit
 
-class Food: FoodItem {
+struct Food: FoodItem, Hashable {
     var foodImage: UIImage
     var foodName: Information
     var foodDescription: Information
     var primeCost: Cost
     var saleCost: Cost
-    
-    init(foodImage: UIImage, foodName: Information, foodDescription: Information,
-         primeCost: Cost, saleCost: Cost) {
-        self.foodImage = foodImage
-        self.foodName = foodName
-        self.foodDescription = foodDescription
-        self.primeCost = primeCost
-        self.saleCost = saleCost
-    }
 }
