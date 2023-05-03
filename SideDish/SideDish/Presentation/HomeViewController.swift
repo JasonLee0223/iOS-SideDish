dc09df1a7948ebc33eb297dc9405c0e7dc59c5d8
Jason <ggj00125@gmail.com>
2023-05-03 20:50:36 +0900

    Feat: CollectionView, superView Layout 메서드 구현

--- SideDish/SideDish/Presentation/HomeViewController.swift
+++ SideDish/SideDish/Presentation/HomeViewController.swift
@@ -25,6 +25,34 @@ class HomeViewController: UIViewController {
         collectionView.backgroundColor = .yellow
         return collectionView
     }()
+//MARK: - Configure Of Layout
+extension HomeViewController {
+    private func configureOfSuperViewLayout() {
+        let superViewSafeArea = self.view.safeAreaLayoutGuide
+        self.view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: superViewSafeArea.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: superViewSafeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: superViewSafeArea.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: superViewSafeArea.bottomAnchor)
+        ])
+    }
+    
+    private func configureOfCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(343),
+                                              heightDimension: .absolute(130))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(343),
+                                               heightDimension: .estimated(1200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
 
