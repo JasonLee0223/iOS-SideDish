5e439eaadef6000ad42b0bc25b865da9fa09b9dd
Jason <ggj00125@gmail.com>
2023-05-06 13:15:37 +0900

    Feat: HomeCollectionViewDataSource 파일 생성 및 enum Section 타입 정의

---
+++ SideDish/SideDish/Presentation/HomeCollectionViewDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewDataSource.swift
+//  SideDish
+//
+//  Created by Jason on 2023/05/06.
+//
+
+import Foundation
+enum Section: CaseIterable {
+    case main
+    case soup
+    case sideDish
+}
