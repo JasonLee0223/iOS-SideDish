ca85f69d794ad233516f271b7d499be5af470c3f
Jason <ggj00125@gmail.com>
2023-06-13 00:32:36 +0900

    [Build]: Section 그룹으로 파일 생성 및 이동

---
+++ SideDish/SideDish/Presentation/CellLayout/Section/Section.swift
@@ -0,0 +1,30 @@
+//
+//  Section.swift
+//  SideDish
+//
+//  Created by Jason on 2023/06/13.
+//
+
+import Foundation
+
+enum Section: Int, CaseIterable {
+    case main = 0
+    case soup
+    case side
+    
+    var headerTitle: String {
+        switch self {
+        case .main: return "모두가 좋아하는 \n든든한 메인 요리"
+        case .soup: return "정선이 담긴 \n뜨끈뜨끈 국물 요리"
+        case .side: return "식탁을 풍성하게 하는 정갈한 밑반찬"
+        }
+    }
+    
+    var offerMenuName: String {
+        switch self {
+        case .main: return APIMagicLiteral.main
+        case .soup: return APIMagicLiteral.soup
+        case .side: return APIMagicLiteral.side
+        }
+    }
+}
