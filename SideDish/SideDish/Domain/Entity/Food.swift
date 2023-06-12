2960007392515e598343dfc153eb41b3bae72854
Jason <ggj00125@gmail.com>
2023-06-13 00:28:09 +0900

    [Style]: 코드 이동

--- SideDish/SideDish/Domain/Entity/Food.swift
+++ SideDish/SideDish/Domain/Entity/Food.swift
@@ -5,11 +5,21 @@
 //  Created by Jason on 2023/04/30.
 //
 
-import Foundation
 import UIKit
 
 struct Food: FoodItem, Hashable {
     var foodImage: UIImage?
     var foodInformation: Information
     var cost: Cost
+}
+
+
+internal struct Information: Hashable {
+    let foodName: String
+    let foodDescription: String
+}
+
+internal struct Cost: Hashable {
+    let primeCost: String
+    let saleCost: String
 }
