71191760fd6e7684b66ef082de577663356c6d1a
Jason <ggj00125@gmail.com>
2023-06-14 16:56:10 +0900

    [Refactor]: Observable 형태 변경으로 인한 적용

--- SideDish/SideDish/Presentation/ViewModel/HomeViewModel.swift
+++ SideDish/SideDish/Presentation/ViewModel/HomeViewModel.swift
@@ -9,12 +9,12 @@ import Foundation
 
 final class HomeViewModel {
     
-    var sectionStorage: [Section: Observable<Food>]
+    var sectionStorage: [Section: Observable<[Food]>]
     
     init() {
         networkService = NetworkService()
         
-        sectionStorage = [.main: Observable<Food>(), .soup: Observable<Food>(), .side: Observable<Food>()]
+        sectionStorage = [.main: Observable<[Food]>(), .soup: Observable<[Food]>(), .side: Observable<[Food]>()]
     }
     
     private let networkService: NetworkService
