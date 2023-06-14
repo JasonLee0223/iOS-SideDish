df4de03b744da66634d7f3e55cfa17187dbb7b61
Jason <ggj00125@gmail.com>
2023-06-14 16:57:41 +0900

    [Refactor]: mockData를 사용한 DataSource에서 API와 관련된 코드로 변경

--- SideDish/SideDish/Presentation/DataSource/HomeCollectionViewDataSource.swift
+++ SideDish/SideDish/Presentation/DataSource/HomeCollectionViewDataSource.swift
@@ -9,25 +9,23 @@ import UIKit
 
 final class HomeCollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
-    private var headers: [Section] = [Section.main, Section.soup, Section.side]
-    
-    private var foods: [Section: [Food]] = [:]
+    override init() {
+        homeViewModel = HomeViewModel()
+    }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return Section.allCases.count
+        return homeViewModel.countOfSection()
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
+        var verifyToItemsCount = 0
         
-        switch self.headers[section] {
-        case .main:
-            return foods[Section.main]?.count ?? 0
-        case .soup:
-            return foods[Section.soup]?.count ?? 0
-        case .side:
-            return foods[Section.side]?.count ?? 0
+        Task {
+            verifyToItemsCount  = try await homeViewModel.countItem(of: section)
         }
+        
+        return verifyToItemsCount
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -42,7 +40,10 @@ final class HomeCollectionViewDataSource: NSObject, UICollectionViewDataSource {
                 return UICollectionReusableView()
             }
             
-            headerView.setTitle(headerText: headers[indexPath.section].headerTitle)
+            Section.allCases.forEach { _ in
+                headerView.setTitle(headerText: Section(rawValue: indexPath.section)?.headerTitle)
+            }
+            
             return headerView
         default:
             assert(false, "Invalid UICollectionReusableView")
@@ -57,33 +58,34 @@ final class HomeCollectionViewDataSource: NSObject, UICollectionViewDataSource {
             return UICollectionViewCell()
         }
         
-        print(headers[indexPath.section])
-        
-        switch self.headers[indexPath.section] {
-        case .main:
-            return configure(cell: cell, food: foods[Section.main]?[indexPath.row])
-        case .soup:
-            return configure(cell: cell, food: foods[Section.soup]?[indexPath.row])
-        case .side:
-            return configure(cell: cell, food: foods[Section.side]?[indexPath.row])
+        Section.allCases.forEach { section in
+            
+            homeViewModel.fetchOfData()
+            
+            guard let foods = homeViewModel.sectionStorage[section]?.value else {
+                return
+            }
+            
+            foods.forEach { food in
+                configure(cell: cell, food: food)
+                collectionView.reloadData()
+            }
         }
+        return cell
     }
     
-    private func configure(cell: HomeViewCell, food: Food?) -> HomeViewCell {
-        guard let food = food else { return HomeViewCell() }
+    private func configure(cell: HomeViewCell, food: Food) {
+        guard let foodImage = UIImage(data: food.foodImage) else {
+            return
+        }
         
-        cell.setFoodImage(image: food.foodImage!)
+        cell.setFoodImage(image: foodImage)
         cell.foodInformationStackView.setTitle(by: food.foodInformation.foodName)
         cell.foodInformationStackView.setDescription(by: food.foodInformation.foodDescription)
         cell.foodInformationStackView.setPrimeCost(by: food.cost.primeCost)
         cell.foodInformationStackView.setDiscountedCost(by: food.cost.saleCost)
 //        cell.menuStackView.setBadges(by: food)
-        return cell
     }
     
-    func fetch(foods: [Food], section: Section) {
-        self.foods[section] = foods
-    }
-    
-    //TODO: - Image 다운로드 및 Configure
+    let homeViewModel: HomeViewModel
 }
