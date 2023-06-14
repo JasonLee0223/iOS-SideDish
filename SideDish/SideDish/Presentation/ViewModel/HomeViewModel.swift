6957702165f472e3ace7dce9a996678e29f62d1b
Jason <ggj00125@gmail.com>
2023-06-14 14:25:38 +0900

    [Feat]: HomeViewModel에 필요한 메서드 구현

--- SideDish/SideDish/Presentation/ViewModel/HomeViewModel.swift
+++ SideDish/SideDish/Presentation/ViewModel/HomeViewModel.swift
@@ -19,3 +19,52 @@ final class HomeViewModel {
     
     private let networkService: NetworkService
 }
+
+//MARK: - Use of DataSource
+extension HomeViewModel {
+    
+    func fetchOfData() {
+        
+        Section.allCases.forEach { section in
+            Task(priority: .background) {
+                await loadAllData(with: section)
+            }
+        }
+    }
+    
+    private func loadAllData(with section: Section) async {
+        var foods = [Food]()
+        
+        do {
+            let networkResult = try await networkService.request(
+                with: APIEndpoint.supplyFoodInformation(with: section.offerMenuName)
+            ).body
+            
+            for foodInformationDTO in networkResult {
+                guard let imageURL = URL(string: foodInformationDTO.foodImage) else {
+                    return
+                }
+                
+                let imageData = try Data(contentsOf: imageURL)
+                
+                let food = Food(
+                    foodImage: imageData,
+                    foodInformation: Information(
+                        foodName: foodInformationDTO.title,
+                        foodDescription: foodInformationDTO.description
+                    ),
+                    cost: Cost(
+                        primeCost: foodInformationDTO.normalPrice ?? "",
+                        saleCost: foodInformationDTO.salePrice ?? ""
+                    )
+                )
+                foods.append(food)
+            }
+            
+        } catch {
+            print("Empty API Data")
+        }
+        
+        sectionStorage[section]?.value = foods
+    }
+}
