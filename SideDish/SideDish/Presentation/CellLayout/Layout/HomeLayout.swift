18bfedf7b4724decaf9ef8ef9a27f43febb5f519
Jason <ggj00125@gmail.com>
2023-06-15 15:16:43 +0900

    [Style]: 코드 위치 변경

--- SideDish/SideDish/Presentation/CellLayout/Layout/HomeLayout.swift
+++ SideDish/SideDish/Presentation/CellLayout/Layout/HomeLayout.swift
@@ -9,15 +9,6 @@ import UIKit
 
 extension HomeViewController {
     
-    func configreOfCollectionViewFlowLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewFlowLayout()
-        let headerSize = CGSize(width: self.view.frame.width, height: 130)
-        let itemSize = CGSize(width:self.view.frame.width, height: 130)
-        layout.headerReferenceSize = headerSize
-        layout.itemSize = itemSize
-        return layout
-    }
-    
     func configureOfCollectionViewCompositionalLayout() -> UICollectionViewLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(343),
                                               heightDimension: .absolute(130))
