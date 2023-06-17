66f9031f854fb4beff8d28ca73647b516e1b1c5d
Jason <ggj00125@gmail.com>
2023-06-17 22:42:41 +0900

    [Feat]: detailFoodData 불러오는 코드 구현

--- SideDish/SideDish/Presentation/Controller/DetailViewController.swift
+++ SideDish/SideDish/Presentation/Controller/DetailViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 final class DetailViewController: UIViewController {
     
     var detailFoodCode: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if animated {
+            guard let unwrappingFoodCode = detailFoodCode else {
+                return
+            }
+            
+            Task {
+                let networkResult = try await networkService.request(
+                    with: APIEndpoint.supplyDetailFoodInformation(
+                        with: APIMagicLiteral.detail, and: unwrappingFoodCode)
+                ).data
+                print(networkResult)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    private let networkService = NetworkService()
 }
