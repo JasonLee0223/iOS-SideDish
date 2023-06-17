5045dfce60cd4544b7742abc5e32aee5fe1dd8b6
Jason <ggj00125@gmail.com>
2023-06-17 22:25:57 +0900

    [Feat]: DetailViewController 파일 생성 및 초기 구현

---
+++ SideDish/SideDish/Presentation/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  SideDish
+//
+//  Created by Jason on 2023/06/17.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    var myTitle: String?
+}
