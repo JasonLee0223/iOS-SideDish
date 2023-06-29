d3bb5f51f04fe9d0b02926de03430a9faf2e3a70
Jason <ggj00125@gmail.com>
2023-06-29 12:51:35 +0900

    [Feat]: Toast 기능 구현
    - 프로퍼티 및 메서드 함께 추가

--- SideDish/SideDish/Presentation/Cells/HomeHeaderView.swift
+++ SideDish/SideDish/Presentation/Cells/HomeHeaderView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Toast
+
 protocol HeaderDelegate: AnyObject {
     
     func didTapSectionHeader(section: HomeHeaderView, sectionNumber: Int)
@@ -18,15 +20,26 @@ final class HomeHeaderView: UICollectionReusableView, Reusable {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configuration()
+        setTapGesture()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        
+        configuration()
+        setTapGesture()
     }
     
     //MARK: - Private Property
     
+    weak var headerDelegate: HeaderDelegate?
+    
+    var itemsCount = 0 {
+        didSet {
+            print("itemsCount = \(itemsCount)")
+        }
+    }
+    
+    private var sectionNumber: Int?
     private let titleFontSize: CGFloat = 32.0
     
     private lazy var sectionTitle: UILabel = {
@@ -59,5 +72,30 @@ extension HomeHeaderView {
 extension HomeHeaderView {
     func setTitle(headerText: String?) {
         sectionTitle.text = headerText
+    }
+    
+    func findSectionNumber(by number: Int) {
+        sectionNumber = number
+    }
+    
+    internal func setTapGesture() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tappedHeaderView))
+        addGestureRecognizer(gesture)
+    }
+    
+    @objc private func tappedHeaderView() {
+        if let sectionNumber = sectionNumber {
+            headerDelegate?.didTapSectionHeader(section: self, sectionNumber: sectionNumber)
+            
+            let toastPoint = CGPoint(x: self.frame.size.width / 3, y: self.frame.size.height)
+    
+            var toastStyle = ToastStyle()
+            toastStyle.backgroundColor = .white
+            toastStyle.activitySize = self.frame.size
+            toastStyle.messageColor = .gray
+    
+            self.makeToast("\(itemsCount)개 상품이 등록되어 있습니다.", point: toastPoint,
+                           title: nil, image: nil, style: toastStyle, completion: nil)
+        }
     }
 }
